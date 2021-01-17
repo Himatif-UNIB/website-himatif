e1f801cd7c7cf2933a76a0fb9307112d451311bb
WahyuS002 <syahputrawahyu61@gmail.com>
2021-01-17 14:35:10 +0700

    Testing tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,35 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: {
+          '500' : '#FFA573',
+          '600' : '#FF9359',
+        },
+
+        'icons': '#9DA3B0',
+
+        'card-color' : '#49566d',
+
+        'dark-blue': '#304352',
+        'dark-blue-800': '#2F3F50',
+        'dark-blue-400': '#92A4B2',
+        'dark-blue-200': '#76848D',
+        'dark-blue-100': '#374D5E',
+
+        'category-text-green': '#75A665',
+        'category-text-yellow': '#A69565',
+        'category-text-blue': '#65A699',
+
+        'category-button-green': '#B5FF9C',
+        'category-button-yellow': '#FFE69C',
+        'category-button-blue': '#9CFFEC',
+      },
+      height: {
+        mammoth : '36rem'
+      },
+    },
   },
   variants: {
     extend: {},
